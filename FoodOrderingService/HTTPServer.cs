7c481a496b68767905dbbc1b1ac35219749f173a
DanielaMazur <danielamazur2000@gmail.com>
2021-10-24 10:46:04 +0300

    Init Food Ordering Service

---
+++ FoodOrderingService/HTTPServer.cs
@@ -0,0 +1,55 @@
+﻿using FoodOrderingService.Services;
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading;
+
+namespace FoodOrderingService
+{
+     class HTTPServer
+     {
+          private bool _isRunning;
+          private HttpListener _httpListener;
+          public const string VERSION = "HTTP/1.1";
+          public const string NAME = "Kitchen";
+
+          public HTTPServer(int port)
+          {
+               _httpListener = new HttpListener();
+               _httpListener.Prefixes.Add($"http://localhost:{port}/");
+          }
+
+          public void Start()
+          {
+               Thread serverThread = new(Run);
+               serverThread.Start();
+          }
+
+          private void Run()
+          {
+               _isRunning = true;
+               _httpListener.Start();
+
+               while (_isRunning)
+               {
+                    HttpListenerContext context = _httpListener.GetContext();
+                    HandleRequest(context);
+               }
+
+               _httpListener.Stop();
+          }
+
+          private void HandleRequest(HttpListenerContext httpContext)
+          {
+               var controllers = new ControllersService().Controllers;
+               var requestedController = controllers.SingleOrDefault(c => c.Method == httpContext.Request.HttpMethod && c.Route == httpContext.Request.RawUrl);
+
+               if (requestedController == null)
+               {
+                    Console.WriteLine("No controller found for the given request.");
+                    return;
+               }
+               requestedController.HandleRequest(httpContext);
+          }
+     }
+}
---
+++ FoodOrderingService/Servicies/ControllersService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using FoodOrderingService.Controllers;
+
+namespace FoodOrderingService.Services
+{
+     class ControllersService
+     {
+          public readonly List<Controller> Controllers = new();
+          public ControllersService()
+          {
+               var controllerTypes = Assembly.GetAssembly(typeof(Controller)).GetTypes()
+                  .Where(controllerType => controllerType.IsClass && !controllerType.IsAbstract && controllerType.IsSubclassOf(typeof(Controller)));
+               foreach (Type controllerType in controllerTypes)
+               {
+                    Controllers.Add((Controller)controllerType.GetProperty("Instance").GetValue(null));
+               }
+          }
+     }
+}
